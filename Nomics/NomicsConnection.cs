ea0c56a2b2941d5762551b668a64c44f8cd3761e
Gordon Brice <gb960@outlook.com>
2018-10-27 14:56:02 -0400

    Initial checkin Nomics connection.

--- Nomics/NomicsConnection.cs
+++ Nomics/NomicsConnection.cs
@@ -7,9 +7,8 @@ namespace Nomics
     public class NomicsConnection
     {
         const string baseUrl = "https://api.nomics.com";
-        const string apiKey = "key=ef81a5ad3480ae8d31b6beb29b7f22e6";
 
-        public async static Task<string> ApiGet(string function)
+        public async static Task<string> ApiGet(string function, string key)
         {
             var response = await Get(string.Format("{0}{1}", baseUrl, function)).ConfigureAwait(false);
 
@@ -25,7 +24,6 @@ namespace Nomics
         {
             using (var client = new HttpClient())
             {
-                client.DefaultRequestHeaders.Add("X-CMC_PRO_API_KEY", "24a1792e-e16d-4a11-ba29-2cd79ed459bb");
                 client.Timeout = TimeSpan.FromSeconds(30);
 
                 return await client.GetAsync(url).ConfigureAwait(false);
